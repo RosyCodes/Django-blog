be9a14960d03908855e07516cecbf296f9fd1592
Rosilie Lim <rosilielim@gmail.com>
2024-06-22 16:04:23 -0500

    Homepage UI modified

--- blog_main/views.py
+++ blog_main/views.py
@@ -32,7 +32,7 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('register')
+            return redirect('login')
         else:
             print(form.errors)
     else:
